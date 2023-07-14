93fe9c45c39b6e4054ffed4c564507cd468d00f9
Medonii <kamil.medon@gmail.com>
2023-07-14 21:01:18 +0200

    added a todo list

---
+++ backend/services/lobby/src/todo.js
@@ -0,0 +1,10 @@
+// TODO: 
+// 1. user can create a lobby and it will automatically assign it to him
+// 2. there is a list of lobbies on the main page
+//     2.1 the listed lobbies show if the game is in progress, the number of people in the lobby/maximum amount, the game that the lobby wants to play
+// 3. in the lobby the users can ready themselves, if all players aren't ready the host can't start the game
+// 4. after ending the game the players go back to the lobby and the history of chat is preserved??? that would be an additional thing
+// 5. the host can kick users
+// 6. the host can make the lobby private and maybe password protected
+
+
